80adb021bfcd5d7aadba741e50e7a4a03e852631
Silverio Dos Santos <silveriojannard@gmail.com>
2022-11-11 22:52:14 -0300

    Primeiro Commit

---
+++ src/main/java/org/senai/atividadebackend/AtividadeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.senai.atividadebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/senai/atividadebackend/AtividadeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.atividadebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
